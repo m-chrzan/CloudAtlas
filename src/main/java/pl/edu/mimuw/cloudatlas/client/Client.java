68b2cba1cf2bc96a996a15220e4df95b2e86520e
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2019-11-14 20:09:45 +0100

    Add basic client

---
+++ src/main/java/pl/edu/mimuw/cloudatlas/client/Client.java
@@ -0,0 +1,42 @@
+package pl.edu.mimuw.cloudatlas.client;
+
+import pl.edu.mimuw.cloudatlas.agent.Api;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class for a client.
+ *
+ * Set the following properties on the command line:
+ *
+ * <br>
+ * -Djava.rmi.server.useCodebaseOnly=false
+ * <br>
+ * -Djava.rmi.server.codebase=file:/path/to/compiled/classes/
+ * <br>
+ * -Djava.security.policy=client.policy
+ * <br>
+ *
+ * <b>NOTE: MAKE SURE YOU HAVE THE TRAILING / ON THE CODEBASE PATH</b>
+ */
+
+// https://github.com/rm5248/Java-RMI-Example/
+
+@SpringBootApplication
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Registry registry = LocateRegistry.getRegistry("localhost");
+            Api api = (Api) registry.lookup("Api");
+
+            SpringApplication.run(Client.class, args);
+        } catch (Exception e) {
+            System.err.println("Client exception:");
+            e.printStackTrace();
+        }
+    }
+}
