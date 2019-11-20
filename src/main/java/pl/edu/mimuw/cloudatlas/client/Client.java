5160a9622c22463a383271e5b00f7b46d6959bb8
Magdalena Grodzińska <mag.grodzinska@gmail.com>
2019-11-20 11:43:09 +0100

    Cleanup Client file

--- src/main/java/pl/edu/mimuw/cloudatlas/client/Client.java
+++ src/main/java/pl/edu/mimuw/cloudatlas/client/Client.java
@@ -1,31 +1,7 @@
 package pl.edu.mimuw.cloudatlas.client;
 
-import org.springframework.context.annotation.Bean;
-import pl.edu.mimuw.cloudatlas.api.Api;
-
-import java.rmi.registry.LocateRegistry;
-import java.rmi.RemoteException;
-import java.rmi.registry.Registry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-/**
- * Main class for a client.
- *
- * Set the following properties on the command line:
- *
- * <br>
- * -Djava.rmi.server.useCodebaseOnly=false
- * <br>
- * -Djava.rmi.server.codebase=file:/path/to/compiled/classes/
- * <br>
- * -Djava.security.policy=client.policy
- * <br>
- *
- * <b>NOTE: MAKE SURE YOU HAVE THE TRAILING / ON THE CODEBASE PATH</b>
- */
-
-// https://github.com/rm5248/Java-RMI-Example/
 
 @SpringBootApplication
 public class Client {
