cf8373137e283736620da5cb225907671fa83d04
Magdalena Grodzińska <mag.grodzinska@gmail.com>
2019-12-22 13:27:20 +0100

    Add task skeleton

---
+++ src/main/java/pl/edu/mimuw/cloudatlas/agent/timer/TimerScheduledTask.java
@@ -0,0 +1,11 @@
+package pl.edu.mimuw.cloudatlas.agent.timer;
+
+import java.util.TimerTask;
+
+public class TimerScheduledTask extends TimerTask {
+
+    @Override
+    public void run() {
+
+    }
+}
