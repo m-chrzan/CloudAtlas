b9f576727fef502fade2370e309d8b238a6a6214
Magdalena Grodzińska <mag.grodzinska@gmail.com>
2019-12-22 13:26:55 +0100

    Add simple scheduler class - no request id or custom time

---
+++ src/main/java/pl/edu/mimuw/cloudatlas/agent/timer/TimerScheduler.java
@@ -0,0 +1,27 @@
+package pl.edu.mimuw.cloudatlas.agent.timer;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Initializes a timer within a constructor during its attachment to the executor
+ * Runs in a thread separate from executor - maybe refactor so that it's attached to executor's thread
+ *
+ * Handle used to attach tasks to schedule
+ * Tasks declared as inherited from TimerTask
+ */
+public class TimerScheduler {
+    private Timer timer;
+
+    TimerScheduler() {
+        this.timer = new Timer();
+    }
+
+    public void handle(TimerTask task, long delay, long period) {
+        this.timer.scheduleAtFixedRate(task, delay, period);
+    }
+
+    public void handle(TimerTask task, long delay) {
+        this.timer.schedule(task, delay);
+    }
+}
