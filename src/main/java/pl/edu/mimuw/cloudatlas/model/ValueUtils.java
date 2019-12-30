c9a84787cd8d3ebb142b11096c5693ca44cfcff2
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2019-12-30 14:24:22 +0100

    Implement ZMI removals in Stanik

--- src/main/java/pl/edu/mimuw/cloudatlas/model/ValueUtils.java
+++ src/main/java/pl/edu/mimuw/cloudatlas/model/ValueUtils.java
@@ -4,4 +4,8 @@ public class ValueUtils {
     public static boolean valueNonNullOfType(Value value, Type type) {
         return value != null && !value.isNull() && value.getType().isCompatible(type);
     }
+
+    public static boolean valueLower(Value a, Value b) {
+        return ((ValueBoolean) a.isLowerThan(b)).getValue();
+    }
 }
--- src/test/java/pl/edu/mimuw/cloudatlas/model/TestUtil.java
+++ src/test/java/pl/edu/mimuw/cloudatlas/model/TestUtil.java
@@ -1,10 +1,6 @@
 package pl.edu.mimuw.cloudatlas.model;
 
 public class TestUtil {
-    public static boolean valueLower(Value a, Value b) {
-        return ((ValueBoolean) a.isLowerThan(b)).getValue();
-    }
-
     public static <T> int iterableSize(Iterable<T> iterable) {
         int count = 0;
         for (T attribute : iterable) {
